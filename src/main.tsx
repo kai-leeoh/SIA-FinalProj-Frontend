5faa4e5bf4e29afc5a0a401f67543d35a38b8733
kai-leeoh <liopogs123@gmail.com>
2026-09-07 22:25:41 +0800

    Fixed OAuth Login Issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ConfigProvider } from "antd";
 import "antd/dist/reset.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import { theme } from "./theme";
 import "./index.css";
+import { theme } from "./theme";
+
+const clientId =
+  "938739633029-kfchnili84oo5vqblfn2vo84q06i0697.apps.googleusercontent.com";
+
+console.log("Google Client ID:", clientId);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConfigProvider theme={theme}>
-      <App />
-    </ConfigProvider>
+    <GoogleOAuthProvider clientId={clientId}>
+      <ConfigProvider theme={theme}>
+        <App />
+      </ConfigProvider>
+    </GoogleOAuthProvider>
   </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      react: resolve("./node_modules/react"),
+      "react-dom": resolve("./node_modules/react-dom"),
+    },
+  },
+});
